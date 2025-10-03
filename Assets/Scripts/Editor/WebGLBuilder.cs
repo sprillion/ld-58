41830aad7360a9a45b5c0f5cb27625151e71c4ca
sprill <sergei.sprill@gmail.com>
2025-10-04 01:36:34 +0500

    first commit

---
+++ Assets/Scripts/Editor/WebGLBuilder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class WebGLBuilder
+{
+   [MenuItem("Build/Build WebGL")]
+   public static void Build()
+   {
+       string[] scenes =
+       {
+           "Assets/Scenes/Game.unity", 
+       };
+
+       BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+       buildPlayerOptions.scenes = scenes;
+       buildPlayerOptions.locationPathName = "Build";
+       buildPlayerOptions.target = BuildTarget.WebGL;
+       buildPlayerOptions.options = BuildOptions.None;
+       
+       var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+       
+       BuildSummary summary = report.summary;
+
+       if (summary.result == BuildResult.Succeeded)
+       {
+           Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+       }
+
+       if (summary.result == BuildResult.Failed)
+       {
+           Debug.Log("Build failed");
+       }
+   }
+}
---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,11 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class TextAnimation : MonoBehaviour
+{
+   private void Awake()
+   {
+      transform.DOScale(1.3f, 0.7f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InQuad);
+   }
+}
