d6c9de915099745cc73f69a3c202c46104675a20
sprill <sergei.sprill@gmail.com>
2025-10-04 02:00:59 +0500

    Test

--- Assets/Scripts/TextAnimation.cs
+++ Assets/Scripts/TextAnimation.cs
@@ -7,5 +7,6 @@ public class TextAnimation : MonoBehaviour
    private void Awake()
    {
       transform.DOScale(1.3f, 0.7f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InQuad);
+      //Для теста
    }
 }
